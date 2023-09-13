14c5cc9770e01e1850425561e87b8a1ed99419e0
diego <maximusdiegoalonsodbz@gmail.com>
2023-09-13 14:23:22 -0600

    Se agregó nuevamente el lector de QR actualizado

--- app/src/main/java/com/miraimx/kinderscontrol/LeerQR_Activity.java
+++ app/src/main/java/com/miraimx/kinderscontrol/LeerQR_Activity.java
@@ -1,8 +1,8 @@
 package com.miraimx.kinderscontrol;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +10,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class LeerQR_Activity extends AppCompatActivity {
     Button btnScan;
@@ -26,28 +27,23 @@ public class LeerQR_Activity extends AppCompatActivity {
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                IntentIntegrator integrator = new IntentIntegrator(LeerQR_Activity.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                integrator.setPrompt("Escanear Código QR");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(true);
-                integrator.initiateScan();
+                ScanOptions options = new ScanOptions();
+                options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+                options.setPrompt("Leer código QR");
+                options.setCameraId(0);  // Use a specific camera of the device
+                options.setBeepEnabled(true);
+                options.setBarcodeImageEnabled(true);
+                barcodeLauncher.launch(options);
             }
         });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null){
-            if (result.getContents() == null){
-                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
-                txtResultado.setText(result.getContents());
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if(result.getContents() == null) {
+                    Toast.makeText(LeerQR_Activity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LeerQR_Activity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
 }
